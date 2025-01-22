a080f4440db335d37481c018e3964260f833f8bf
Jacobloseycoder <loseyjacob1@gmail.com>
2025-01-22 13:31:31 -0600

    Update Yeetzy.py

--- Yeetzy.py
+++ Yeetzy.py
@@ -1,2 +1,30 @@
-Yeetzy
-ggg
+import random as r
+def main():
+    # accepts no arguments
+    # Calls all functions to play a number of games
+    games = input("How many games: ")
+    dice = [1, 2, 3, 4, 5, 6]
+    count_frequency()
+def output_dice(dice):
+    #accepts no arguments
+    #outputs each die in the list
+    for number in dice:
+        print (number)
+        
+def roll_die():
+    #accepts no arguments
+    #Returns a random integer from 1 to 6
+    dice_roll = r.randint(1, 6)
+def first_roll():
+    pass
+def count_frequency(dice, number):
+    pass
+def find_mode(dice):
+    pass
+def list_unmatched_dice(dice, mode):
+    
+    index = 00
+def reroll_one(dice, index):
+    pass
+def reroll_many(dice):
+    pass
